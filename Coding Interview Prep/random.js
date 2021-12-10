021942db9112c2ecdf30d440778ac0309d4161d1
Etienne <etienne.guilhaume@gmail.com>
2021-12-11 00:45:26 +0100

    find longest word

--- Coding Interview Prep/random.js
+++ Coding Interview Prep/random.js
@@ -6,4 +6,17 @@
 function reverseString(str) {
     return [...str].reverse().join('');
   }
-  
+
+/**
+ * Finds the longest word in a string.
+ * 
+ * match: creates an array of words
+ * reduce: reduces the array keeping only the largest string.
+ * .length at the end.
+ * 
+ * @param {*} str initial string.
+ * @returns length of the longest word.
+ */
+function findLongestWordLength(str) {
+  return str.match(/([a-z]+)/ig).reduce((a, b) => a.length > b.length ? a : b).length;
+}
