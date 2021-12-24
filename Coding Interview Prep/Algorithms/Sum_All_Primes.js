92267de1226e7254b972b9b59374f6dcd7428f48
Etienne <etienne.guilhaume@gmail.com>
2021-12-24 01:15:59 +0100

    Update Sum_All_Primes.js

--- Coding Interview Prep/Algorithms/Sum_All_Primes.js
+++ Coding Interview Prep/Algorithms/Sum_All_Primes.js
@@ -9,14 +9,11 @@ function sumPrimesNaive(num) {
 }
 
 function isPrimeNaive(num) {
-  if (num == 1) {
+  if (num == 1 || num % 2 == 0) {
     return false;
   }
   if (num == 2) {
     return true;
-  }
-  if (num % 2 == 0) {
-    return false;
   }
   for (let i = 3; i < num; i += 2) {
     if (num % i == 0) {
@@ -30,27 +27,20 @@ function isPrimeNaive(num) {
 function sumPrimes(num) {
   const arr = [...Array(num).fill(true)];
   let sum = 0;
+  arr[0] = false;
   arr.map((element, index, array) => {
     const number = index + 1;
- //   console.log("Number: " + number);
-    if (index == 0) {
-      array[index] = false;
-    }
     if (element) {
-      if (number > 1) {
-        for (let i = number * number; i <= num; i += number) {
-        //  console.log("THIS IS NOT PRIME: " + i);
-          array[i - 1] = false;
-        }
+      for (let i = number * number; i <= num; i += number) {
+        array[i - 1] = false;
       }
-    } 
+    }
   });
   arr.map((element, index) => {
     if (element == true) {
       sum += index + 1;
     }
   });
-//  console.log(arr);
   return sum;
 }
 
